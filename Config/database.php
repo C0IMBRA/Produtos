75b4748461c82586abe87b319b6be0112be63ebc
Gabriel Coimbra <gabriel.coimbra@beteltecnologia.com.br>
2023-10-26 17:48:57 -0300

    Paulin barata

--- Config/database.php
+++ Config/database.php
@@ -1,15 +1,14 @@
 <?php
-$host = 'localhost'; // Host do banco de dados
-$username = 'root'; // Nome de usuário do banco de dados
-$password = ''; // Senha do banco de dados
-$database = 'coimbra_db'; // Nome do banco de dados
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'coimbra_db';
 
 try {
-    // Criar uma conexão com o banco de dados usando PDO
+
     $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);    
-    // Configurar o PDO para lançar exceções em caso de erros
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // Agora você pode executar consultas no banco de dados usando $conn
+    
 } catch (PDOException $e) {
     die("Erro na conexão: ". $e->getMessage());
 }
--- Controller/ProdutosController.php
+++ Controller/ProdutosController.php
@@ -6,6 +6,7 @@ require_once 'models/Produtos.php';
 class ProdutosController {
     public function adicionar() {
         $produto = new Produtos();
+        include 'Views/Produtos/adicionar.php';
     }
     public function deletar() {
         $produto = new Produtos();
@@ -19,3 +20,15 @@ class ProdutosController {
         $produto = new Produtos();
     }
 }
+
+//arquivo de hosts
+// C:\Windows\System32\drivers\etc\hosts
+//C:\xampp\apache\conf\extra adiconar -> 127.0.0.1 coimbra.com
+
+//<VirtualHost *:80>
+//     ServerAdmin webmaster@dummy-host2.example.com
+//     DocumentRoot "C:/xampp/htdocs/produtos/"
+//     ServerName coimbra.com
+//     ErrorLog "logs/dummy-host2.example.com-error.log"
+//     CustomLog "logs/dummy-host2.example.com-access.log" common
+// </VirtualHost>
